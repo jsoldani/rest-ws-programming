dca97f94dfc1c4af99c186cbfe509e0d7e8a3bba
jsoldani <jsoldani@gmail.com>
2022-10-19 16:07:08 +0200

    hello (with query params)

--- ws-programming/hello-world.py
+++ ws-programming/hello-world.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/hello',methods=['GET'])
 def hello_world():
-    return jsonify({'Hello':'World!'})
+    whom = request.args.get("whom")
+    return jsonify({'Hello':whom})
 
 if __name__ == '__main__':
     app.run(port=50000)
 
 
-    
